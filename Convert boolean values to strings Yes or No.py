f8b13cf8a4615113199dff38de5800c9b6a1158d
bharath g s <royalkingpin@gmail.com>
2017-03-03 20:19:03 +0530

    yes or no

---
+++ Convert boolean values to strings Yes or No.py
@@ -0,0 +1,8 @@
+"""Given: a boolean value
+Return: a 'Yes' string for true and a 'No' string for false"""
+
+def bool_to_word(bool):
+    if bool == True:
+        return "Yes"
+    else:
+        return "No"

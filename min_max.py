13069e88ac2b363398816550f21e48ebfe78a411
bharath g s <royalkingpin@gmail.com>
2017-03-04 08:35:45 +0530

    min_max

---
+++ min_max.py
@@ -0,0 +1,10 @@
+"""Your task is to make two functions,
+max and min (maximum and minimum in PHP) that take a(n)
+array/vector of integers list as input and outputs, respectively,
+the largest and lowest number in that array/vector."""
+
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)

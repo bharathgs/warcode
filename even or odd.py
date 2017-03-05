07576ecbfc289f34b7ed4b0255c1a99e246b0f1c
bharath g s <royalkingpin@gmail.com>
2017-03-05 21:01:47 +0530

    8kyu

---
+++ even or odd.py
@@ -0,0 +1,6 @@
+"""Create a function that takes an integer as an argument
+and returns "Even" for even numbers or "Odd" for odd numbers."""
+
+def even_or_odd(number):
+    return 'Even' if number % 2 == 0 else 'Odd'
+

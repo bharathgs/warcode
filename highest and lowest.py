216a5aed40f27e3644d5f1995913c76d66015956
bharath g s <royalkingpin@gmail.com>
2017-03-05 18:17:17 +0530

    high and low

--- highest and lowest.py
+++ highest and lowest.py
@@ -1,6 +1,7 @@
 """In this little assignment you are given a string of space separated numbers,
 and have to return the highest and lowest number."""
 
+
 def high_and_low(numbers):
     number_list = [int(i) for i in numbers.split(" ")]
     return "{} {}".format(max(number_list), min(number_list))

6154aee7017af96b2e20adf75329ad2769d442ba
bharath g s <royalkingpin@gmail.com>
2017-03-07 11:00:30 +0530

    7 kyu

---
+++ repeater.py
@@ -0,0 +1,5 @@
+'''Write a function named repeater() that takes two arguments (a string and an integer),
+and returns a new string where the input string is repeated that many times.'''
+
+def repeater(string, n):
+    return string*n

40e455e29263fda564531127f19bfe425c61251b
bharath g s <royalkingpin@gmail.com>
2017-03-09 18:48:28 +0530

    another 7kyu

--- WeIrD StRiNg CaSe.py
+++ WeIrD StRiNg CaSe.py
@@ -14,3 +14,12 @@ def to_weird_case(s):
     for char in s:
         weird += char
     return weird
+
+'''
+also we could do this:
+
+def to_weird_case_word(string):
+    return "".join(c.upper() if i%2 == 0 else c for i, c in enumerate(string.lower()))
+    
+def to_weird_case(string):
+    return " ".join(to_weird_case_word(str) for str in string.split())'''
